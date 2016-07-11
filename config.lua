cc23fcd3d7e62562c0d2238f94178ff7219481d6
Allwen <AllwenWaker81@gmail.com>
2016-07-11 02:50:20 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,7 +10,7 @@ return {
 	},
 	log_chat = -1001069634662,
 	channel = '@AllwenTeam', --channel username with the '@'
-	help_group = 'https://telegram.me/joinchat/CHYUej9jFxFN12MQqrLnUg', --group link, not username!
+	help_group = 'https://telegram.me/joinchat/DYf3Fj6_rT8UAroF-QWkxQ', --group link, not username!
 	languages = 'languages.lua',
 	plugins = {
 		'onmessage.lua', --THIS HAVE TO BE THE FIRST: IF AN USER IS SPAMMING/IS BLOCKED, THE BOT WON'T GO THROUGH PLUGINS
